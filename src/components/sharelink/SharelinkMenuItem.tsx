6c1eeaa87f5e6db080ab26b6c57ffc66bcbbdf41
Leo Dastur <leo@siftstack.com>
2025-10-13 12:55:00 -0700

    create menubar options

--- src/components/sharelink/SharelinkMenuItem.tsx
+++ src/components/sharelink/SharelinkMenuItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IconButton, InlineLabel, Menu, WithContextMenu } from '@grafana/ui';
+import { Button, InlineLabel, Menu, WithContextMenu } from '@grafana/ui';
+import siftLogo from '../../img/logo.svg';
 
 export interface ShareLinkItem {
   assetId?: string;
@@ -17,13 +18,24 @@ export const SharelinkMenuItem = ({ className, items: _items }: SharelinkMenuIte
     <InlineLabel width="auto" transparent className={className} style={{ marginLeft: 'auto' }}>
       <WithContextMenu
         renderMenuItems={() => (
-          <Menu.Group label="Test">
-            <Menu.Item label="First" />
-            <Menu.Item label="Second" />
+          <Menu.Group label="Open in Sift">
+            <Menu.Item label="Open Link" />
+            <Menu.Item label="Copy to Clipboard" />
           </Menu.Group>
         )}
       >
-        {({ openMenu }) => <IconButton name="info-circle" onClick={openMenu} />}
+        {({ openMenu }) => (
+          <Button
+            onClick={openMenu}
+            size="md"
+            fill="text"
+            variant="secondary"
+            aria-label="Open in Sift"
+            tooltip="Open this query in Sift's explorer view"
+          >
+            <img src={siftLogo} alt="Sift" style={{ width: 20, height: 20 }} />
+          </Button>
+        )}
       </WithContextMenu>
     </InlineLabel>
   );

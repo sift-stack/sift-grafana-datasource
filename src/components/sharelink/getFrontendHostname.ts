1bd6fec84b61bc0ba054c072d636f9293fd05790
Leo Dastur <leo@siftstack.com>
2025-10-24 16:16:02 -0700

    clarify getFrontendHostname.ts

--- src/components/sharelink/getFrontendHostname.ts
+++ src/components/sharelink/getFrontendHostname.ts
@@ -1,3 +1,7 @@
+
+
+// The sift frontend can be hosted on multiple domains, and there's no dynamic way to get the correct hostname from the backend api
+// Edit this file to test the sharelink against other endpoings
 export function getFrontendHostname(apiBaseUrl: string): string | null {
   if (!apiBaseUrl) {
     return null;
